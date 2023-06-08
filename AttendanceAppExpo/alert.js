64f8b238f82ea9bff67e0fad593d05e310bde1f4
Alex Cheah <alexkcheah@gmail.com>
2023-06-08 14:50:19 +0800

    initialise react native CLI project

---
+++ AttendanceAppExpo/alert.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { Feather } from '@expo/vector-icons';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Button, TouchableOpacity, FlatList } from 'react-native';
+import IconButton from './iconButton';
+
+const Alert = () => {
+  const handleButtonPress = () => {
+    alert('Successfully checked in!');
+  };
+
+return (
+  <View>
+    <IconButton
+      buttonIcon="check"
+      label="CHECK IN"
+      name="JASON CHUNG"
+      onPress={handleButtonPress}
+      captionIcon="map-pin"
+      caption="LOCATION -"
+      />
+  </View>
+);
+};
+
+export default Alert;

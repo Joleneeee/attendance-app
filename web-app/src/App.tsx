ea506d371a415d3c7f9a0631ea9be816eeb794b2
Joleneeee <117433588+Joleneeee@users.noreply.github.com>
2023-07-10 10:28:57 +0800

    Update App.tsx

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,174 +1,150 @@
-// import React, { useState } from 'react';
-// import './App.css'
+import React, { useState } from 'react';
+import './App.css'
 
-// const Login = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [emailError, setEmailError] = useState('');
+// const subjects = [
+//   { id: 1, name: 'PRG2014' },
+//   { id: 2, name: 'SEG2102' },
+//   { id: 3, name: 'PRG3014' }
+// ];
 
-//   const handleEmailChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-//     setEmail(e.target.value);
-//     setEmailError('');
+// const AttendanceCode = () => {
+//   const [attendanceCode, setAttendanceCode] = useState('');
+//   const [students, setStudents] = useState([]);
+
+//   const generateAttendanceCode = () => {
+//     // Generate a random 5-digit code
+//     const code = Math.floor(10000 + Math.random() * 90000).toString();
+//     setAttendanceCode(code);
+//     setStudents([]);
 //   };
 
-//   const handlePasswordChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-//     setPassword(e.target.value);
+//   const handleCheckIn = (student: any) => {
+//     setStudents((prevStudents) => [...prevStudents, student]);
 //   };
-
-//   const validateEmail = (email: string) => {
-//     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     return regex.test(email);
-//   };
-
-//   const handleSubmit = (e: { preventDefault: () => void; }) => {
-//     e.preventDefault();
-
-//     if (!validateEmail(email)) {
-//       setEmailError('Please enter a valid email address.');
-//       return;
-//     }
-
-//     // Perform authentication logic here
-//     // For simplicity, let's assume the login is successful
-//     setIsLoggedIn(true);
-//   };
-
-//   if (isLoggedIn) {
-//     return <div>You are logged in!</div>;
-//   }
 
 //   return (
-//     <div className="image">
-//     <img src="/sunwaylogo.jpg" className="image" alt="Sunway University logo" />
-//     <div className="container">
-//       <h2 >Login Page</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div className="container">
-//           <label>Email: </label>
-//           <input type="email" value={email} onChange={handleEmailChange} />
-//           {emailError && <div className="error">{emailError}</div>}
-//         </div>
-//         <div className="container">
-//           <label>Password: </label>
-//           <input
-//             type="password"
-//             value={password}
-//             onChange={handlePasswordChange}
-//           />
-//         </div>
-//         <div className= "loginButton"></div>
-//         <button type="submit">Login</button>
-//       </form>
-//     </div>
+//     <div>
+//       <h3>Attendance Code: {attendanceCode}</h3>
+//       <button onClick={generateAttendanceCode}>Generate Attendance Code</button>
+//       <h4>Students</h4>
+//       <ul>
+//         {students.map((student, index) => (
+//           <li key={index}>{student}</li>
+//         ))}
+//       </ul>
 //     </div>
 //   );
 // };
 
-// export default Login;
+// const SubjectList = ({ subjects }) => {
+//   const [selectedSubject, setSelectedSubject] = useState(null);
 
-import React, { useState } from 'react';
+//   const handleSubjectClick = (subject) => {
+//     setSelectedSubject(subject);
+//   };
 
-const subjects = [
-  { id: 1, name: 'PRG2014' },
-  { id: 2, name: 'SEG2102' },
-  { id: 3, name: 'PRG3014' }
-];
+//   return (
+//     <div>
+//       <h2>Subjects</h2>
+//       <ul>
+//         {subjects.map((subject) => (
+//           <li key={subject.id} onClick={() => handleSubjectClick(subject)}>
+//             {subject.name}
+//           </li>
+//         ))}
+//       </ul>
+//       {selectedSubject && <AttendanceCode />}
+//     </div>
+//   );
+// };
 
-const AttendanceCode = () => {
-  const [attendanceCode, setAttendanceCode] = useState('');
-  const [students, setStudents] = useState([]);
-
-  const generateAttendanceCode = () => {
-    // Generate a random 5-digit code
-    const code = Math.floor(10000 + Math.random() * 90000).toString();
-    setAttendanceCode(code);
-    setStudents([]);
-  };
-
-  const handleCheckIn = (student) => {
-    setStudents((prevStudents) => [...prevStudents, student]);
-  };
-
-  return (
-    <div>
-      <h3>Attendance Code: {attendanceCode}</h3>
-      <button onClick={generateAttendanceCode}>Generate Attendance Code</button>
-      <h4>Students</h4>
-      <ul>
-        {students.map((student, index) => (
-          <li key={index}>{student}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const SubjectList = ({ subjects }) => {
-  const [selectedSubject, setSelectedSubject] = useState(null);
-
-  const handleSubjectClick = (subject) => {
-    setSelectedSubject(subject);
-  };
-
-  return (
-    <div>
-      <h2>Subjects</h2>
-      <ul>
-        {subjects.map((subject) => (
-          <li key={subject.id} onClick={() => handleSubjectClick(subject)}>
-            {subject.name}
-          </li>
-        ))}
-      </ul>
-      {selectedSubject && <AttendanceCode />}
-    </div>
-  );
-};
-
-const LoginForm = ({ handleLogin }) => {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [emailError, setEmailError] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleEmailChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setEmail(e.target.value);
+    setEmailError('');
+  };
+
+  const handlePasswordChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setPassword(e.target.value);
+  };
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
-    handleLogin();
+
+    if (!validateEmail(email)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    }
+
+    // Perform authentication logic here
+    // For simplicity, let's assume the login is successful
+    setIsLoggedIn(true);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
-  );
-};
-
-const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setLoggedIn(true);
-  };
+  if (isLoggedIn) {
+    return <div>You are logged in!</div>;
+  }
 
   return (
-    <div className="App">
-      <h1>Lecturer Login</h1>
-      {loggedIn ? (
-        <SubjectList subjects={subjects} />
-      ) : (
-        <LoginForm handleLogin={handleLogin} />
-      )}
+    <div>
+    <img
+      src="/sunwaylogo.jpg"
+      className="image"
+      alt="Sunway University logo"
+    />
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="container">
+          <label>Email: </label>
+          <input type="email" value={email} onChange={handleEmailChange} />
+          {emailError && <div className="error">{emailError}</div>}
+        </div>
+        <div className="container">
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className= "loginButton"></div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
     </div>
   );
 };
+
+
+// const App = () => {
+//   const [loggedIn, setLoggedIn] = useState(false);
+
+//   const handleLogin = () => {
+//     setLoggedIn(true);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Lecturer Login</h1>
+//       {loggedIn ? (
+//         <SubjectList subjects={subjects} />
+//       ) : (
+//         <LoginForm handleLogin={handleLogin} />
+//       )}
+//     </div>
+//   );
+// };
+
+export default Login;
+
+

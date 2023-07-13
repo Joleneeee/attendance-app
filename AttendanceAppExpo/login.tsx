543ae135d004158d8c4a8270ae890e351762c8ee
Alex Cheah <alexkcheah@gmail.com>
2023-07-13 22:59:23 +0800

    fix: student login

--- AttendanceAppExpo/login.tsx
+++ AttendanceAppExpo/login.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import { View, Image, TextInput, Button, StyleSheet } from 'react-native';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { View, Image, TextInput, Button, StyleSheet } from "react-native";
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ navigation }: any) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleEmailChange = (value) => {
     setEmail(value);
@@ -13,44 +15,44 @@ const Login = () => {
     setPassword(value);
   };
 
-  const handleLogin = () => {
-    const validEmail = 'student@example.com';
-    const validPassword = 'password';
-    console.log('Email:', email);
-    console.log('Password:', password);
+  const getUserAuth = async () => {
+    const response = await axios.get(`http://192.168.0.110:3000/student/${email}/${password}`);
+    const data = response.data[0];
+    console.log(data);
+    return data;
+  };
 
-    // Reset email and password fields after login
-    setEmail('');
-    setPassword('');
+  const handleLogin = async () => {
 
-    if (email === validEmail && password === validPassword) {
-        console.log('Login successful');
-        // Navigate to the next screen or perform any other actions upon successful login
-        setEmail('');
-        setPassword('');
-      } else {
-        console.log('Invalid email or password');
-        // Display an error message or perform any other actions upon unsuccessful login
+    const data = getUserAuth();
+
+    if (await data) {
+      setIsLoggedIn(true);
+      if(isLoggedIn) {
+        navigation.navigate('Checkin');
+        alert("Login Successful")
       }
-    };
+    } else {
+      alert("Login Failed")
+      console.log("Invalid Student Id or password");
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Image
-        source={require('./sunwaylion.jpeg')}
+        source={require("./sunwaylion.jpeg")}
         style={styles.logo}
         resizeMode="contain"
       />
       <TextInput
         style={styles.input}
         placeholder="Email"
-        value={email}
         onChangeText={handleEmailChange}
       />
       <TextInput
         style={styles.input}
         placeholder="Password"
-        value={password}
         onChangeText={handlePasswordChange}
         secureTextEntry
       />
@@ -62,18 +64,18 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   logo: {
     width: 200,
     height: 200,
   },
   input: {
-    width: '80%',
+    width: "80%",
     height: 40,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     marginBottom: 10,
     paddingHorizontal: 10,
   },
